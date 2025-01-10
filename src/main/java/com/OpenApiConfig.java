a986c990c9eb0293316ce6efce93c18e7acabf83
dongwk <dwk126@126.com>
2025-01-10 18:11:26 +0800

    feat(api-docs): 分组接口文档
    - 注释掉全局的 packages-to-scan 和 paths-to-match 配置
    - 新增 site 和 web 接口的分组配置
    - 使用 GroupedOpenApi 进行接口文档的分组

--- src/main/java/com/OpenApiConfig.java
+++ src/main/java/com/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +25,19 @@ public class OpenApiConfig {
                         .description("SpringBoot 集成 Knife4j 示例"));
     }
 
-    
+    @Bean
+    public GroupedOpenApi siteApi() {
+        return GroupedOpenApi.builder()
+                .group("site接口")
+                .packagesToScan("com")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi webnApi() {
+        return GroupedOpenApi.builder()
+                .group("web接口")
+                .packagesToScan("com")
+                .build();
+    }
 }
