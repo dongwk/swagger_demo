10c7320a9297e3c787ff8f3c6402587a6bdbefdc
dongwk <dwk126@126.com>
2025-01-10 15:40:06 +0800

    refactor: 恢复 HelloController 并注释掉 API 文档路径

--- src/main/java/com/contr/HelloController.java
+++ src/main/java/com/contr/HelloController.java
@@ -1,4 +1,4 @@
-//package com.contr;
+package com.contr;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RequestMapping("/test")
-//@RestController
-//@Tag(name = "HelloController11", description = "Hello Controller22")
-//public class HelloController {
-//
-//    @Operation(summary = "Hello World44444")
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "Hello, World!";
-//    }
-//}
+@RequestMapping("/test")
+@RestController
+@Tag(name = "HelloController11", description = "Hello Controller22")
+public class HelloController {
+
+    @Operation(summary = "Hello World44444")
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
