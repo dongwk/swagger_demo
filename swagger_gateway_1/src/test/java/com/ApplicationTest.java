b28b65165441c639d708af63096899c94878a8b2
dongwk <dwk126@126.com>
2025-01-13 19:33:31 +0800

    feat: 添加 swagger_gateway_1模块
    - 新增 swagger_gateway_1 模块，实现 Swagger UI 和 Knife4j 增强配置
    - 添加 Spring Boot应用启动类和测试类
    - 配置 Gateway 路由
    - 引入 Swagger3 和 Knife4j 相关依赖

---
+++ swagger_gateway_1/src/test/java/com/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dongwk
+ * @date 2018/11/26 17:00
+ **/
+@SpringBootTest(classes={Application.class})// 指定启动类
+public class ApplicationTest {
+
+}
